b05629abdc17c90fcb7e5a21e99d69f54d40fcba
tom2ram <tom2ram@gmail.com>
2023-06-04 11:36:43 +0530

    updated with String arguments

--- src/com/durga/predefine/functionalinterface/Predicate/PredicateDemo1.java
+++ src/com/durga/predefine/functionalinterface/Predicate/PredicateDemo1.java
@@ -11,5 +11,11 @@ public class PredicateDemo1
 		System.out.println(p.test(12));
 		System.out.println(p.negate().test(12));
 		System.out.println(p.negate().test(2));
+		//System.out.println(p.test("Durga")); CE : incompatible type, String can not be convert to Integer
+		
+		String s = "khalid";
+		Predicate<String> p1 = a -> a == s;
+		System.out.println(p1.test("durga"));
+		System.out.println(p1.test("khalid"));
 	}
 }
